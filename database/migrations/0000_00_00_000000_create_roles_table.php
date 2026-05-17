b5479a9e8a8dfb4997393ac9d7aad6403c334be2
Muhammed Enes <enessoftwareeng@gmail.com>
2026-05-17 22:55:10 +0300

    favicon eklendi.

--- database/migrations/0000_00_00_000000_create_roles_table.php
+++ database/migrations/0000_00_00_000000_create_roles_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 50); // <-- Muhtemelen bu satır eksik!
+            $table->string('name', 50);
             $table->timestamps();
         });
     }
